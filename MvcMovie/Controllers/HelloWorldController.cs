293a4ef3a2773b6426ba4340fc905cbfcbae19a9
yadavn01 <ynaman29@gmail.com>
2024-06-09 18:33:38 +1200

    view setup wip

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -6,9 +6,9 @@ namespace MvcMovie.Controllers;
 public class HelloWorldController : Controller
 {
     // GET: /HelloWorld/
-    public string Index()
+    public IActionResult Index()
     {
-        return "This is my default action...";
+        return View();
     }
 
     //Get: /HellowWorld/Welcome/
