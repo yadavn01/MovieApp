37b43183cc81c5456ff3243145bcc09cf8381605
yadavn01 <ynaman29@gmail.com>
2024-06-10 12:02:38 +1200

    model setup and nuget packages installed

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -12,8 +12,10 @@ public class HelloWorldController : Controller
     }
 
     //Get: /HellowWorld/Welcome/
-    public String Welcome(string name, int numTimes = 1)
+    public IActionResult Welcome(string name, int numTimes)
     {
-        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        ViewData["Message"] = "Hello" + name;
+        ViewData["NumTimes"] = numTimes;
+        return View();
     }
 }
