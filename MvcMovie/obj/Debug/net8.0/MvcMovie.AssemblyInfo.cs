5b1e42870e912fc6841233a6a84fc768eeb7d2e1
yadavn01 <ynaman29@gmail.com>
2024-06-17 12:15:51 +1200

    title filtering added

--- MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa0ffebdc38e50a44af3c14b6f62c0216136b937")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba92250091a4c061bf7afe306dfbf473c92101dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
