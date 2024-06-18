3b4dd9fd9ecfed6eda8ef94ca541934533fa533a
yadavn01 <ynaman29@gmail.com>
2024-06-18 12:18:14 +1200

    unused param added in delete action

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -166,7 +166,7 @@ namespace MvcMovie.Controllers
         // POST: Movies/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id, bool notUsed)
         {
             var movie = await _context.Movie.FindAsync(id);
             if (movie != null)
